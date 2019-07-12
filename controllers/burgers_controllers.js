dfb2b0f4ba06c21a6b594499d0557776e8f54478
Marisa Quijano <miss.quijano@gmail.com>
2019-07-11 22:29:33 -0400

    controllers

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -2,32 +2,30 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/cat.js");
+var burgers = require("../models/burgers.js");
 
-// Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burgers.all(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/cats", function(req, res) {
-  cat.create([
-    "name", "sleepy"
+router.post("/api/burgers", function(req, res) {
+  burgers.create([
+    "burgers", "yum"
   ], [
-    req.body.name, req.body.sleepy
+    req.body.name, req.body.hungry
   ], function(result) {
-    // Send back the ID of the new quote
+   
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
@@ -36,26 +34,23 @@ router.put("/api/cats/:id", function(req, res) {
     sleepy: req.body.sleepy
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
+    return res.status(404).end();
     } else {
       res.status(200).end();
     }
   });
 });
 
-router.delete("/api/cats/:id", function(req, res) {
+router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  burgers.delete(condition, function(result) {
     if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
+     return res.status(404).end();
     } else {
       res.status(200).end();
     }
   });
 });
 
-// Export routes for server.js to use.
 module.exports = router;
