f728ece06801bc46360182d6cc14fc60663600fd
Marisa Quijano <miss.quijano@gmail.com>
2019-07-11 22:34:36 -0400

    adding seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('Bob');
+INSERT INTO burgers (name, yum) VALUES ('Linda', true);
+INSERT INTO burgers (name, yum) VALUES ('Tina', true);
+INSERT INTO burgers (name, yum) VALUES ('Gene', true);
+INSERT INTO burgers (name) VALUES ('Louise');
